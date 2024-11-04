8a309dd5b7784f71f435222a127bf073741d1ee8
Tchi <tchi.devica@gmail.com>
2024-11-05 02:23:07 +0300

    chore: about responsive

--- src/app/about/_components/about-hero.tsx
+++ src/app/about/_components/about-hero.tsx
@@ -7,17 +7,19 @@ import {
 } from '@/components/ui/carousel';
 import { Badge } from 'lucide-react';
 import Image from 'next/image';
+
 const images = ['/465530959.jpg', '/465530248.jpg'];
+
 export const AboutHero = () => (
   <div className='w-full py-20 lg:py-40'>
     <div className='container mx-auto'>
-      <div className='grid grid-cols-1 items-end justify-end gap-10 lg:grid-cols-2'>
-        <div className='flex flex-col items-center gap-4 lg:items-start'>
+      <div className='grid grid-cols-1 items-end justify-end gap-10 xl:grid-cols-2'>
+        <div className='flex flex-col items-center gap-4 px-2 xl:items-start'>
           <div>
             <Badge>Platform</Badge>
           </div>
           <div className='flex flex-col gap-2'>
-            <h2 className='font-regular text-center text-xl tracking-tighter md:text-3xl lg:max-w-xl lg:text-left lg:text-5xl'>
+            <h2 className='font-regular text-center text-xl tracking-tighter md:text-3xl lg:max-w-xl lg:text-5xl xl:text-left'>
               9,8 pour un séjour à deux
             </h2>
             <p className='max-w-xl text-center text-lg leading-relaxed tracking-tight text-muted-foreground lg:text-left'>
@@ -30,7 +32,7 @@ export const AboutHero = () => (
             </p>
           </div>
         </div>
-        <div className='w-full max-w-full px-6'>
+        <div className='flex max-w-full justify-center'>
           <Carousel className='w-[700px]'>
             <CarouselContent>
               {images.map((image) => (
@@ -45,8 +47,8 @@ export const AboutHero = () => (
                 </CarouselItem>
               ))}
             </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
+            <CarouselPrevious className='absolute left-7' />
+            <CarouselNext className='absolute right-7' />
           </Carousel>
         </div>
       </div>
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,41 +1,74 @@
 'use server';
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from '@/components/ui/carousel';
 import { AboutHero } from './_components/about-hero';
+import Image from 'next/image';
+
+const images = ['/e1.jpg', '/e.jpg', '/m.jpg', '/m1.jpg'];
 
 export default async function About() {
   return (
     <main>
       <AboutHero />
-      <section className='flex w-full flex-col items-center justify-center py-10 md:items-center md:justify-center lg:py-20'>
+      <div>
         <h4 className='font-regular text-center text-2xl tracking-tighter md:text-3xl lg:text-4xl'>
           Selon les clients, la description et les photos de cet établissement
           reflètent vraiment la réalité.
         </h4>
-        <p className='mt-4 max-w-xl text-left text-base leading-relaxed tracking-tight text-muted-foreground'>
-          Offrant une vue sur la montagne, ce chalet comprend une terrasse, un
-          coin salon, une télévision par satellite à écran plat, une cuisine
-          entièrement équipée avec un lave-vaisselle et un micro-ondes ainsi
-          qu'une salle de bains privative pourvue d'une douche et d'un
-          sèche-cheveux. Un grille-pain, un réfrigérateur, des plaques de
-          cuisson, une machine à café et une bouilloire sont également fournis.
-          Le linge de lit et les serviettes sont fournis.
-        </p>
-        <p className='mt-4 max-w-xl text-left text-base leading-relaxed tracking-tight text-muted-foreground'>
-          En été, vous pourrez utiliser le barbecue et dîner sur le balcon
-          privé.
-        </p>
-        <p className='mt-4 max-w-xl text-left text-base leading-relaxed tracking-tight text-muted-foreground'>
-          Un service de location de vélos est assuré sur place pour vous
-          permettre de partir à la découverte des environs. Vous pourrez
-          pratiquer le ski et le vélo dans les environs. Un service de location
-          de matériel de ski, un point de vente de forfaits de ski et un local à
-          skis sont également disponibles sur place.
-        </p>
-        <p className='mt-4 max-w-xl text-left text-base leading-relaxed tracking-tight text-muted-foreground'>
-          L'aéroport le plus proche, celui de Mulhouse-Fribourg, est situé à 75
-          km.
-        </p>
-      </section>
+        <section className='flex w-full flex-col-reverse items-center justify-center px-4 py-10 lg:py-20 xl:flex-row-reverse'>
+          <div className='px-2'>
+            <p className='mt-4 max-w-xl text-left text-base leading-relaxed tracking-tight text-muted-foreground'>
+              Offrant une vue sur la montagne, ce chalet comprend une terrasse,
+              un coin salon, une télévision par satellite à écran plat, une
+              cuisine entièrement équipée avec un lave-vaisselle et un
+              micro-ondes ainsi qu'une salle de bains privative pourvue d'une
+              douche et d'un sèche-cheveux. Un grille-pain, un réfrigérateur,
+              des plaques de cuisson, une machine à café et une bouilloire sont
+              également fournis. Le linge de lit et les serviettes sont fournis.
+            </p>
+            <p className='mt-4 max-w-xl text-left text-base leading-relaxed tracking-tight text-muted-foreground'>
+              En été, vous pourrez utiliser le barbecue et dîner sur le balcon
+              privé.
+            </p>
+            <p className='mt-4 max-w-xl text-left text-base leading-relaxed tracking-tight text-muted-foreground'>
+              Un service de location de vélos est assuré sur place pour vous
+              permettre de partir à la découverte des environs. Vous pourrez
+              pratiquer le ski et le vélo dans les environs. Un service de
+              location de matériel de ski, un point de vente de forfaits de ski
+              et un local à skis sont également disponibles sur place.
+            </p>
+            <p className='mt-4 max-w-xl text-left text-base leading-relaxed tracking-tight text-muted-foreground'>
+              L'aéroport le plus proche, celui de Mulhouse-Fribourg, est situé à
+              75 km.
+            </p>
+          </div>
+          <div className='flex max-w-full justify-center'>
+            <Carousel className='w-[700px]'>
+              <CarouselContent>
+                {images.map((image) => (
+                  <CarouselItem key={image}>
+                    <Image
+                      src={image}
+                      alt='tytyty'
+                      className='flex aspect-video items-center justify-center rounded-md bg-muted p-6'
+                      width={1000}
+                      height={900}
+                    />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious className='absolute left-7' />
+              <CarouselNext className='absolute right-7' />
+            </Carousel>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
