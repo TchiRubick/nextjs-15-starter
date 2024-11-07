a02ed000d05f75c2ed19c38090fd3833e496ecb7
Tchi <tchi.devica@gmail.com>
2024-11-07 10:08:10 +0300

    chore: checkin design

--- src/app/check-in/page.tsx
+++ src/app/check-in/page.tsx
@@ -6,11 +6,34 @@ import { useSearchParamsServerParser as searchParamsServerParser } from '@/hooks
 import InternalError from '@/lib/error';
 import { getAvailability } from '@packages/uplisting';
 import Image from 'next/image';
+import { MapPin, BedSingle, Bed, Bath, House } from 'lucide-react';
 import {
   defaultParamsValidation,
   paramsValidation,
   type ParamsValidation,
 } from './_validations';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from '@/components/ui/card';
+import { Label } from '@radix-ui/react-label';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import Link from 'next/link';
 
 const CheckInPage = async ({
   searchParams,
@@ -45,46 +68,67 @@ const CheckInPage = async ({
         </div>
       )}
       {Array.isArray(availability) && availability.length > 0 && (
-        <div className='flex flex-col gap-4'>
+        <div className='flex flex-row gap-4'>
           {availability.map((item) => (
-            <ul key={item.id}>
-              <li>name: {item.name}</li>
-              <li>description: {item.description}</li>
-              <li>capacity: {item.capacity}</li>
-              <li>bedrooms: {item.rooms.bedrooms}</li>
-              <li>beds: {item.rooms.beds}</li>
-              <li>bathrooms: {item.rooms.bathrooms}</li>
-              <li>
-                address: {item.address.street}, {item.address.city},{' '}
-                {item.address.state}, {item.address.zip_code},{' '}
-                {item.address.country}
-              </li>
-              <li>
-                amenities:
-                <ol>
-                  {item.amenities.map((amenity) => (
-                    <li key={amenity.group}>
-                      {amenity.group}: {amenity.values.join(', ')}
-                    </li>
-                  ))}
-                </ol>
-              </li>
-              <li>
-                photos:
-                <ol>
-                  {item.photos.map((photo) => (
-                    <li key={photo.url}>
-                      <Image
-                        src={photo.url}
-                        alt={photo.created_at}
-                        width={100}
-                        height={100}
-                      />
-                    </li>
-                  ))}
-                </ol>
-              </li>
-            </ul>
+            <Card key={item.id} className='w-64 rounded-3xl'>
+              <CardHeader className='p-0'>
+                <CardTitle>
+                  <Carousel>
+                    <CarouselContent>
+                      {item.photos.map((photo) => (
+                        <CarouselItem key={photo.url}>
+                          <Image
+                            src={photo.url}
+                            alt={photo.created_at}
+                            width={200}
+                            height={200}
+                            className='h-60 w-full rounded-t-3xl object-cover'
+                          />
+                        </CarouselItem>
+                      ))}
+                    </CarouselContent>
+                    <CarouselPrevious className='absolute left-2 h-5 w-5' />
+                    <CarouselNext className='absolute right-2 h-5 w-5' />
+                  </Carousel>
+                </CardTitle>
+              </CardHeader>
+              <Link href={`/check-in/${item.id}`}>
+                <CardContent className='flex flex-col space-y-3 p-2'>
+                  <Label className='text-lg font-bold'>{item.name}</Label>
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger className='flex font-serif text-sm'>
+                        <MapPin className='mr-2 h-4 w-4' />
+                        <span className='overflow-hidden text-ellipsis text-nowrap'>
+                          {item.address.street}, {item.address.city},
+                          {item.address.state}, {item.address.zip_code},
+                          {item.address.country}
+                        </span>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        {item.address.street}, {item.address.city},
+                        {item.address.state}, {item.address.zip_code},
+                        {item.address.country}
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </CardContent>
+                <CardFooter className='flex h-fit justify-center space-x-5 pb-2 pt-6 text-sm'>
+                  <span className='flex'>
+                    <Bed className='mr-2 h-4 w-4' />{' '}
+                    <span className='font-bold'>{item.rooms.bedrooms} </span>
+                  </span>
+                  <span className='flex'>
+                    <Bath className='mr-2 h-4 w-4' />
+                    <span className='font-bold'>{item.rooms.bathrooms} </span>
+                  </span>
+                  <span className='flex'>
+                    <House className='mr-2 h-4 w-4' />
+                    <span className='font-bold'>{item.rooms.bedrooms}</span>
+                  </span>
+                </CardFooter>
+              </Link>
+            </Card>
           ))}
         </div>
       )}
