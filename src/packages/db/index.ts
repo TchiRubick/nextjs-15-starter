cbb3c636c45239e95de4cdcb672bd678f4c936f0
Tchi <tchi.devica@gmail.com>
2024-11-13 00:53:19 +0300

    chore: railway test (#1)
    * chore: migrate to node-postgres
    
    * chore:logout admin
    
    ---------
    
    Co-authored-by: Tchi <noreply@github.com>
    Co-authored-by: TophyLad <tophyrakotomavo@gmail.com>

--- src/packages/db/index.ts
+++ src/packages/db/index.ts
@@ -1,8 +1,6 @@
 import { env } from '@/env';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './src/schema';
-
-export { alias } from 'drizzle-orm/pg-core';
 
 export { schema };
 
