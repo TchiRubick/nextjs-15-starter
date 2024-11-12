4e053ae99a82ddf5c1761115f77a8d177494efe0
TophyLad <tophyrakotomavo@gmail.com>
2024-11-12 11:10:12 +0300

    refactor: auth

--- src/app/(public)/auth/(signin)/page.tsx
+++ src/app/(public)/auth/(signin)/page.tsx
@@ -1,73 +1,11 @@
-'use client';
-
-import { login } from '@/actions/auth';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-
-import Link from 'next/link';
+import { pageOrphanGuard } from '@packages/auth/index';
+import { SigninForm } from './signin-form';
 
 // TODO: use pageOrphanGuard to prevent a connected user from accessing this page
 
-const SigninPage = () => {
-  const handleSignin = async (formData: FormData) => {
-    await login(formData);
-    window.location.replace('/');
-  };
-
-  return (
-    <form action={handleSignin}>
-      <Card className='mx-auto max-w-sm'>
-        <CardHeader>
-          <CardTitle className='text-2xl'>Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className='grid gap-4'>
-            <div className='grid gap-2'>
-              <Label htmlFor='email'>Email or username</Label>
-              <Input
-                name='identifier'
-                id='identifier'
-                placeholder='m@example.com'
-                required
-              />
-            </div>
-            <div className='grid gap-2'>
-              <div className='flex items-center'>
-                <Label htmlFor='password'>Password</Label>
-                <Link
-                  href='forgotpassword'
-                  className='ml-auto inline-block text-sm underline'
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input name='password' id='password' type='password' required />
-            </div>
-            <Button type='submit' className='w-full'>
-              Login
-            </Button>
-          </div>
-          <div className='mt-4 text-center text-sm'>
-            Don&apos;t have an account?{' '}
-            <Link href='/auth/signup' className='underline'>
-              Sign up
-            </Link>
-          </div>
-        </CardContent>
-      </Card>
-    </form>
-  );
+const SigninPage = async () => {
+  await pageOrphanGuard();
+  return <SigninForm />;
 };
 
 export default SigninPage;
--- src/app/(public)/auth/signup/page.tsx
+++ src/app/(public)/auth/signup/page.tsx
@@ -1,76 +1,11 @@
-import Link from 'next/link';
-
-import { register } from '@/actions/auth';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { pageOrphanGuard } from '@packages/auth/index';
+import { SignupForm } from './signup-form';
 
 // TODO: use pageOrphanGuard to prevent a connected user from accessing this page
 
-const SignupPage = () => {
-  const handleSignup = async (formData: FormData) => {
-    await register(formData);
-    window.location.replace('/');
-  };
-
-  return (
-    <form action={handleSignup}>
-      <Card className='mx-auto max-w-sm'>
-        <CardHeader>
-          <CardTitle className='text-2xl'>Sign up</CardTitle>
-          <CardDescription>
-            Enter your username, email and password below to create your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className='grid gap-4'>
-            <div className='grid gap-2'>
-              <Label htmlFor='user'>Username</Label>
-              <Input
-                id='user'
-                type='text'
-                placeholder='user'
-                required
-                name='username'
-              />
-            </div>
-            <div className='grid gap-2'>
-              <Label htmlFor='email'>Email</Label>
-              <Input
-                id='email'
-                type='email'
-                placeholder='m@example.com'
-                required
-                name='email'
-              />
-            </div>
-            <div className='grid gap-2'>
-              <div className='flex items-center'>
-                <Label htmlFor='password'>Password</Label>
-              </div>
-              <Input id='password' type='password' required name='password' />
-            </div>
-            <Button type='submit' className='w-full'>
-              Sign up
-            </Button>
-          </div>
-          <div className='mt-4 text-center text-sm'>
-            Have you already an account?{' '}
-            <Link href='/auth' className='underline'>
-              Signin
-            </Link>
-          </div>
-        </CardContent>
-      </Card>
-    </form>
-  );
+const SignupPage = async () => {
+  await pageOrphanGuard();
+  return <SignupForm />;
 };
 
 export default SignupPage;
