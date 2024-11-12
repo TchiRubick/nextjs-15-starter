e42d4b6c863d6a13978de60c865f5f1f91cabd27
TophyLad <tophyrakotomavo@gmail.com>
2024-11-13 01:14:54 +0300

    chore: multi-select component

--- src/app/admin/products/create/_components/create-product-form.tsx
+++ src/app/admin/products/create/_components/create-product-form.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { AMENITIES_QUERY_KEY } from "@/app/admin/amenities/static";
-import { Button } from "@/components/ui/button";
+import { AMENITIES_QUERY_KEY } from '@/app/admin/amenities/static';
+import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
@@ -9,17 +9,18 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { toast } from "@/hooks/use-toast";
-import { CreateProduct } from "@/types";
-import { useMutationAction } from "@packages/fetch-action/index";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { PRODUCTS_QUERY_KEY } from "../../static";
-import { createProductAction, getAmenitiesAction } from "../action";
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { toast } from '@/hooks/use-toast';
+import { CreateProduct } from '@/types';
+import { useMutationAction } from '@packages/fetch-action/index';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { PRODUCTS_QUERY_KEY } from '../../static';
+import { createProductAction, getAmenitiesAction } from '../action';
+import { MultiSelect } from '@/app/admin/_components/multi-select';
 
 export const CreateProductForm = () => {
   const queryClient = useQueryClient();
@@ -37,28 +38,28 @@ export const CreateProductForm = () => {
       queryClient.invalidateQueries({ queryKey: PRODUCTS_QUERY_KEY });
 
       toast({
-        title: "Product created successfully",
+        title: 'Product created successfully',
       });
 
       router.push('/admin/products');
     },
     onError: () => {
       toast({
-        title: "Failed to create product",
+        title: 'Failed to create product',
       });
     },
   });
 
   const form = useForm<CreateProduct>({
     defaultValues: {
-      name: "",
-      description: "",
+      name: '',
+      description: '',
       price: 0,
       room: 1,
       bed: 0,
       bath: 0,
       maxPerson: 0,
-      status: "draft",
+      status: 'draft',
     },
   });
 
@@ -68,15 +69,15 @@ export const CreateProductForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
         <FormField
           control={form.control}
-          name="name"
+          name='name'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Name</FormLabel>
               <FormControl>
-                <Input placeholder="Product name" {...field} />
+                <Input placeholder='Product name' {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -85,12 +86,12 @@ export const CreateProductForm = () => {
 
         <FormField
           control={form.control}
-          name="description"
+          name='description'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Description</FormLabel>
               <FormControl>
-                <Textarea placeholder="Product description" {...field} />
+                <Textarea placeholder='Product description' {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -99,17 +100,19 @@ export const CreateProductForm = () => {
 
         <FormField
           control={form.control}
-          name="price"
+          name='price'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Price</FormLabel>
               <FormControl>
                 <Input
-                  onFocus={e => e.target.value === "0" && (e.target.value = "")}
-                  type="number"
-                  placeholder="0"
+                  onFocus={(e) =>
+                    e.target.value === '0' && (e.target.value = '')
+                  }
+                  type='number'
+                  placeholder='0'
                   {...field}
-                  onChange={e => field.onChange(Number(e.target.value))}
+                  onChange={(e) => field.onChange(Number(e.target.value))}
                 />
               </FormControl>
               <FormMessage />
@@ -117,20 +120,22 @@ export const CreateProductForm = () => {
           )}
         />
 
-        <div className="grid grid-cols-2 gap-4">
+        <div className='grid grid-cols-2 gap-4'>
           <FormField
             control={form.control}
-            name="room"
+            name='room'
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Rooms</FormLabel>
                 <FormControl>
                   <Input
-                    onFocus={e => e.target.value === "0" && (e.target.value = "")}
-                    type="number"
+                    onFocus={(e) =>
+                      e.target.value === '0' && (e.target.value = '')
+                    }
+                    type='number'
                     min={1}
                     {...field}
-                    onChange={e => field.onChange(Number(e.target.value))}
+                    onChange={(e) => field.onChange(Number(e.target.value))}
                   />
                 </FormControl>
                 <FormMessage />
@@ -140,17 +145,19 @@ export const CreateProductForm = () => {
 
           <FormField
             control={form.control}
-            name="bed"
+            name='bed'
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Beds</FormLabel>
                 <FormControl>
                   <Input
-                    onFocus={e => e.target.value === "0" && (e.target.value = "")}
-                    type="number"
+                    onFocus={(e) =>
+                      e.target.value === '0' && (e.target.value = '')
+                    }
+                    type='number'
                     min={0}
-                    {...field}
-                    onChange={e => field.onChange(Number(e.target.value))}
+                    value={field.value ?? ''}
+                    onChange={(e) => field.onChange(Number(e.target.value))}
                   />
                 </FormControl>
                 <FormMessage />
@@ -160,17 +167,19 @@ export const CreateProductForm = () => {
 
           <FormField
             control={form.control}
-            name="bath"
+            name='bath'
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Bathrooms</FormLabel>
                 <FormControl>
                   <Input
-                    onFocus={e => e.target.value === "0" && (e.target.value = "")}
-                    type="number"
+                    onFocus={(e) =>
+                      e.target.value === '0' && (e.target.value = '')
+                    }
+                    type='number'
                     min={0}
-                    {...field}
-                    onChange={e => field.onChange(Number(e.target.value))}
+                    value={field.value ?? ''}
+                    onChange={(e) => field.onChange(Number(e.target.value))}
                   />
                 </FormControl>
                 <FormMessage />
@@ -180,17 +189,19 @@ export const CreateProductForm = () => {
 
           <FormField
             control={form.control}
-            name="maxPerson"
+            name='maxPerson'
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Max Persons</FormLabel>
                 <FormControl>
                   <Input
-                    onFocus={e => e.target.value === "0" && (e.target.value = "")}
-                    type="number"
+                    onFocus={(e) =>
+                      e.target.value === '0' && (e.target.value = '')
+                    }
+                    type='number'
                     min={0}
-                    {...field}
-                    onChange={e => field.onChange(Number(e.target.value))}
+                    value={field.value ?? ''}
+                    onChange={(e) => field.onChange(Number(e.target.value))}
                   />
                 </FormControl>
                 <FormMessage />
@@ -199,10 +210,17 @@ export const CreateProductForm = () => {
           />
         </div>
 
-        <Button type="submit" disabled={isPending}>
-          {isPending ? "Creating..." : "Create Product"}
+        <Button type='submit' disabled={isPending}>
+          {isPending ? 'Creating...' : 'Create Product'}
         </Button>
       </form>
+      <MultiSelect
+        options={[
+          { value: 'option1', label: 'Option 1' },
+          { value: 'option2', label: 'Option 2' },
+        ]}
+        onValueChange={(values) => console.log(values)}
+      />
     </Form>
   );
-}
+};
