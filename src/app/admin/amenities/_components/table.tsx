f86798db1a6a5e67b2f9ea909ee103e07e802dab
TophyLad <tophyrakotomavo@gmail.com>
2024-11-12 22:43:13 +0300

    chore: action delete amenity

--- src/app/admin/amenities/_components/table.tsx
+++ src/app/admin/amenities/_components/table.tsx
@@ -6,6 +6,22 @@ import { useQuery } from '@tanstack/react-query';
 import { ColumnDef } from '@tanstack/react-table';
 import { getAllAmenitiesAction } from '../action';
 import { AMENITIES_QUERY_KEY } from '../static';
+import { Button } from '@/components/ui/button';
+import { Loader2, Trash2 } from 'lucide-react';
+import { useMutationAction } from '@packages/fetch-action/index';
+import { deleteAmenityAction } from '../action';
+import { useQueryClient } from '@tanstack/react-query';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { DialogTrigger } from '@radix-ui/react-dialog';
+
 const columns: ColumnDef<AmenitySelect>[] = [
   {
     accessorKey: 'id',
@@ -14,6 +30,55 @@ const columns: ColumnDef<AmenitySelect>[] = [
   {
     accessorKey: 'name',
     header: 'Name',
+  },
+  {
+    accessorKey: 'action',
+    header: 'Action',
+    cell: ({ row }) => {
+      const { mutateAsync, isPending } = useMutationAction(deleteAmenityAction);
+      const queryClient = useQueryClient();
+
+      const onRemove = async (id: number) => {
+        await mutateAsync(id);
+        await queryClient.invalidateQueries({ queryKey: AMENITIES_QUERY_KEY });
+      };
+      return (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              variant='ghost'
+              className='z-50 bg-none'
+              disabled={isPending}
+            >
+              {isPending ? (
+                <Loader2 className='animated-spin' />
+              ) : (
+                <Trash2 className='h-4 w-4' color='red' />
+              )}
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete this
+                amenity.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button>Cancel</Button>
+              </DialogClose>
+              <DialogClose asChild>
+                <Button onClick={() => onRemove(row.original.id)}>
+                  Confirm
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      );
+    },
   },
 ];
 
--- src/app/admin/amenities/action.ts
+++ src/app/admin/amenities/action.ts
@@ -4,6 +4,7 @@ import InternalError from '@/lib/error';
 import {
   AmenitySelect,
   createAmenity,
+  deleteAmenity,
   getAllAmenities,
   InsertAmenity,
 } from '@packages/db/models/amenities';
@@ -21,5 +22,16 @@ export const createAmenityAction = async (
     return amenity;
   } catch (error) {
     return new InternalError(error, 'CREATION_FAILED');
+  }
+};
+
+export const deleteAmenityAction = async (
+  id: number
+): Promise<AmenitySelect | InternalError> => {
+  try {
+    const [amenity] = await deleteAmenity(id);
+    return amenity;
+  } catch (error) {
+    return new InternalError(error, 'DELETION_FAILED');
   }
 };
