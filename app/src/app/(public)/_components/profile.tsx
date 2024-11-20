e8381fa29d5d1938afb459147384d149f10d93cb
anderson-suiriex <121947055+anderson-suiriex@users.noreply.github.com>
2024-11-20 17:50:19 +0300

    fix: Testimonials responsive and correction (#11)
    * chore: update user
    
    * chore:testimonials responsive and correction

--- app/src/app/(public)/_components/profile.tsx
+++ app/src/app/(public)/_components/profile.tsx
@@ -15,23 +15,21 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import { userUpdate } from '@/actions/user.action';
-import { useEffect } from 'react';
 
 export function Profile() {
   const { data } = useSession();
   const { register, getValues, handleSubmit } = useForm();
 
-  const { mutateAsync, error, isError } = useMutationAction(userUpdate);
+  const { mutateAsync, error, isError } =
+    useMutationAction(userUpdate);
 
-  useEffect(() => {
-    if (isError) {
-      toast({
-        variant: 'destructive',
-        title: 'Error',
-        description: error?.message,
-      });
-    }
-  }, [isError, error?.message]);
+  if (isError) {
+    toast({
+      variant: 'destructive',
+      title: 'Error',
+      description: error?.message,
+    });
+  }
 
   const onSubmit = async () => {
     const formData = formDataBuilder(getValues);
@@ -42,80 +40,47 @@ export function Profile() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className='flex justify-center pt-20 sm:pt-40'>
-        <Card className=''>
-          <h2 className='text-center text-2xl font-bold text-black'>
-            Modifier votre profile{' '}
-          </h2>
-          <div className='flex flex-col sm:flex-row sm:gap-5'>
-            <CardHeader className='self-center'>
-              <CardTitle className='text-2xl'>
-                <Label
-                  htmlFor='imageavatar'
-                  className='relative cursor-pointer hover:opacity-50'
-                >
-                  <Avatar className='h-40 w-40'>
-                    <AvatarImage
-                      src='/gallery/p.jpg'
-                      width={500}
-                      height={500}
-                    />
-                  </Avatar>
-                  <Input id='imageavatar' className='hidden' type='file' />
+    <div className='flex justify-center pt-20 sm:pt-40'>
+      <Card className='flex flex-col sm:flex-row sm:gap-5'>
+        <CardHeader className='self-center'>
+          <CardTitle className='text-2xl'>
+            <Avatar className='h-40 w-40'>
+              <AvatarImage src='/gallery/p.jpg' width={500} height={500} />
+            </Avatar>
+          </CardTitle>
+          <CardDescription className='self-center'>
+            Usernameexemple
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className='grid gap-4 sm:pt-10'>
+            <div className='flex flex-row gap-2'>
+              <Label htmlFor='email' className='self-center'>
+                UserName:
+              </Label>
+              <Input id='username' placeholder='username' required defaultValue={data?.username} {...register('username')} />
+            </div>
+            <div className='grid gap-2'>
+              <div className='flex flex-row gap-2'>
+                <Label htmlFor='email' className='self-center'>
+                  Email:
                 </Label>
-              </CardTitle>
-              <CardDescription className='self-center text-xl font-bold text-black'>
-                {data?.username}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className='grid gap-4 sm:pt-10'>
-                <div className='flex flex-row gap-2'>
-                  <Label htmlFor='username' className='self-center'>
-                    UserName:
-                  </Label>
-                  <Input
-                    id='username'
-                    placeholder='username'
-                    required
-                    defaultValue={data?.username}
-                    {...register('username')}
-                  />
-                </div>
-                <div className='grid gap-2'>
-                  <div className='flex flex-row gap-2'>
-                    <Label htmlFor='email' className='self-center'>
-                      Email:
-                    </Label>
-                    <Input
-                      id='email'
-                      placeholder='email@gmail.com'
-                      value={data?.email}
-                      required
-                      disabled
-                    />
-                  </div>
-                </div>
-                <div className='flex flex-row gap-2'>
-                  <Label htmlFor='adresse' className='self-center'>
-                    Adresse:
-                  </Label>
-                  <Input id='adresse' placeholder='adresse' />
-                </div>
-                <div className='flex flex-row gap-2'>
-                  <Label htmlFor='telephone' className='self-center'>
-                    Téléphone:
-                  </Label>
-                  <Input id='telephone' placeholder='telephone' />
-                </div>
-                <Button type='submit' className='w-full'>
-                  Enregistrer
-                </Button>
+                <Input
+                  id='username'
+                  placeholder='email@gmail.com'
+                  value={data?.email}
+                  required
+                  disabled
+                />
               </div>
-            </CardContent>
+            </div>
+            <Button type='submit' className='w-full'>
+              Save
+            </Button>
           </div>
-        </Card>
-      </div>
+        </CardContent>
+      </Card>
+    </div>
     </form>
   );
 }
