ae2346dd912c4027c837320dd174c9b6ae569057
TophyLad <tophyrakotomavo@gmail.com>
2024-11-21 18:54:34 +0300

    chore: list schedule user

--- app/src/app/[locale]/(public)/_components/reservation-recap.tsx
+++ app/src/app/[locale]/(public)/_components/reservation-recap.tsx
@@ -18,6 +18,7 @@ import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import { MoveRight } from 'lucide-react';
 import Link from 'next/link';
+import { format } from 'date-fns';
 
 export const ReservationRecap = () => {
   const tReservationRecap = useScopedI18n('reservationRecap');
