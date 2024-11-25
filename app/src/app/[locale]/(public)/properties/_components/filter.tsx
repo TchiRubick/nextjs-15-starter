98859914bb23671d25b5f563c22ad422ca2ca79d
andersonsuiriex <andersonsuiriex@gmail.com>
2024-11-25 21:27:08 +0300

    chore:responsive filter

--- app/src/app/[locale]/(public)/properties/_components/filter.tsx
+++ app/src/app/[locale]/(public)/properties/_components/filter.tsx
@@ -85,13 +85,13 @@ export const Filter = ({
       onSubmit={handleSubmit(onSubmit)}
       className='mx-4 rounded-xl bg-emerald-950 p-6 shadow-xl sm:mx-auto'
     >
-      <div className='flex flex-wrap items-end gap-6'>
+      <div className='flex flex-col gap-y-5 md:flex-row md:gap-x-4 '>
         {/* Price Range */}
-        <div className='min-w-[200px] flex-1'>
+        <div className=' md:basis-1/2'>
           <Label className='mb-2 block text-sm font-medium text-white/80'>
             {tFilter('priceLabel')}
           </Label>
-          <div className='flex gap-3'>
+          <div className='flex sm:flex-row sm:gap-4 flex-col gap-4 '>
             <div className='relative flex-1'>
               <span className='absolute left-3 top-1/2 -translate-y-1/2 text-white'>
                 €
@@ -124,7 +124,8 @@ export const Filter = ({
         </div>
 
         {/* Date Range */}
-        <div className='w-full'>
+
+        <div className='flex flex-col pr-2 md:basis-1/4'>
           <Label className='mb-2 block text-sm font-medium text-white/80'>
             {tFilter('dateRangeLabel')}
           </Label>
@@ -138,8 +139,8 @@ export const Filter = ({
                 minDate={new Date()}
                 format='DD-MM-YYYY'
                 range
-                className='green w-full! date-full-width'
-                inputClass='w-full! h-12 border-0 bg-white/30 cursor-pointer pl-8 text-white placeholder:text-slate-400 focus:ring-2 focus:ring-primary rounded-md'
+                className=''
+                inputClass=' w-full md:w-[115%] text-center h-12 border-0 bg-white/30 cursor-pointer  text-white placeholder:text-slate-400 focus:ring-2 focus:ring-primary rounded-md'
               />
             )}
           />
@@ -149,10 +150,10 @@ export const Filter = ({
         <Button
           type='submit'
           size='lg'
-          className='h-12 w-full min-w-[140px] border-foreground/20 bg-slate-100 text-primary/90 transition-all hover:translate-y-[-2px] hover:bg-slate-200 hover:shadow-lg active:translate-y-0 sm:w-full lg:w-[48px]'
+          className='h-12 md:ml-5 md:basis-1/4 md:mt-7   min-w-[140px] border-foreground/20 bg-slate-100 text-primary/90 transition-all hover:translate-y-[-2px] hover:bg-slate-200 hover:shadow-lg active:translate-y-0 sm:w-full lg:w-[40px]'
           disabled={!isDirty}
         >
-          <Search className='mr-2 h-4 w-4' />
+          <Search className='mr-2 h-4 w-4 ' />
           {tFilter('searchButton')}
         </Button>
       </div>
