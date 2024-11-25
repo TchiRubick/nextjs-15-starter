b76a74bbde638cfdfb1f233bd3dac96818d215d5
Aro Fortunat <aro.fortunat.ralaiseheno@esti.mg>
2024-11-25 18:02:49 +0300

    fix:pull

--- app/src/app/[locale]/(public)/pictures/page.tsx
+++ app/src/app/[locale]/(public)/pictures/page.tsx
@@ -2,10 +2,9 @@ import { getUrlImageQuery } from '@/actions/image.action';
 import { Gallery } from './_components/gallery';
 
 const Pictures = async () => {
-    
-    const images =  await getUrlImageQuery()
-    
-    return <Gallery images={images} />;
-}
+  const images = await getUrlImageQuery();
+
+  return <Gallery images={images} />;
+};
 
 export default Pictures;
